1153117e9c7a2e8e90655861238031e1421911fd
dhiru909 <dhirajkhali1@gmail.com>
2024-04-15 23:58:44 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("I am a hero")
+import { log } from "console";
+import app from "./src/app";
+
+
+const startServer = () =>{
+    const port = process.env.PORT || 3000;
+    app.listen(port,()=>{
+        log(`Listening on port ${port}`);
+    });
+};
+
+startServer();
