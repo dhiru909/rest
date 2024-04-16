4c31f568c01293ba08df6118e27614f17fb670a7
dhiru909 <dhirajkhali1@gmail.com>
2024-04-17 00:36:00 +0530

    created router and controller for user

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 const app = express();
 
 app.get("/", (req, res, next) => {
@@ -9,6 +10,8 @@ app.get("/", (req, res, next) => {
   throw error;
   res.json({ message: "welcome" });
 });
+
+app.use("/api/users",userRouter);
 
 //global error handler at last of all routes
 app.use(globalErrorHandler);
