69d2aba8aa998e8e52f8cbd119413df08f7b305a
dhiru909 <dhirajkhali1@gmail.com>
2024-05-01 02:31:36 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,15 @@ import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+); // allow all origins for development purposes only
+// app.options("*", cors()) // enable preflight requests for CORS
 app.get("/", (req, res, next) => {
   // const error = createHttpError(500, "something went wrong");
   // throw error;
@@ -14,9 +21,7 @@ app.get("/", (req, res, next) => {
 
 //routers
 app.use("/api/users", userRouter);
-app.use("/api/books",bookRouter);
-
-
+app.use("/api/books", bookRouter);
 
 //global error handler at last of all routes
 app.use(globalErrorHandler);
--- src/config/config.ts
+++ src/config/config.ts
@@ -9,6 +9,7 @@ const _config = {
   cloudinaryName: process.env.CLOUDINARY_NAME || 'dummy',
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY || 'dummy',
   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET || 'dummy',
+  frontendDomain:process.env.FRONTEND_DOMAIN
 };
 
 export const config = Object.freeze(_config);
