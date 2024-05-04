46214547acfec035e30710346721bdf3e59c4ce6
dhiru909 <dhirajkhali1@gmail.com>
2024-05-05 03:11:07 +0530

    config prettier

--- elib-client-app/src/app/layout.tsx
+++ elib-client-app/src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>This is a navbar</nav>
+        {children}
+      </body>
     </html>
   );
 }
--- elib-client-app/src/app/page.tsx
+++ elib-client-app/src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <h1>Dhiraj khali</h1>
-  );
+    return (
+        <>
+            <h1>Welcome to Elib Client Application</h1>
+        </>
+    )
 }
