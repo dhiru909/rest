d4e9fbaeb507556efcd5b6fd3373e54900a2c9e4
dhiru909 <dhirajkhali1@gmail.com>
2024-05-12 21:54:25 +0530

    re organize components

--- elib-client-app/src/app/(home)/page.tsx
+++ elib-client-app/src/app/(home)/page.tsx
@@ -1,9 +1,10 @@
+import Banner from '@/app/(home)/components/Banner'
 import Image from 'next/image'
 
 export default function Home() {
     return (
         <>
-            <h1>Welcome to Elib Client Application</h1>
+            <Banner />
         </>
     )
 }
